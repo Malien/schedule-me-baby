89633aac5322d36a3e05c1c8747cfb63d4ddb276
Yaroslav <q.link0.p@gmail.com>
2021-09-19 10:54:15 +0300

    initial commit

---
+++ src/main/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/ScheduleMeBabyApplication.kt
@@ -0,0 +1,11 @@
+package ua.edu.ukma.dudes.scheduleMeBaby
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScheduleMeBabyApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScheduleMeBabyApplication>(*args)
+}
---
+++ src/test/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/ScheduleMeBabyApplicationTests.kt
@@ -0,0 +1,13 @@
+package ua.edu.ukma.dudes.scheduleMeBaby
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScheduleMeBabyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
