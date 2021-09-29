734e01b38d2b1751c229e59105c381618dcb2c97
BlaD200 <blad600@gmail.com>
2021-09-29 23:29:16 +0300

    Added save and delete for subjects

--- src/main/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/controller/SubjectController.kt
+++ src/main/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/controller/SubjectController.kt
@@ -1,10 +1,8 @@
 package ua.edu.ukma.dudes.scheduleMeBaby.controller
 
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import ua.edu.ukma.dudes.scheduleMeBaby.dto.SubjectDTO
 import ua.edu.ukma.dudes.scheduleMeBaby.entity.Subject
 import ua.edu.ukma.dudes.scheduleMeBaby.service.SubjectService
 import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
@@ -14,14 +12,33 @@ import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 class SubjectController(private val subjectService: SubjectService) {
 
     @GetMapping("/all")
-    fun getAllSubjects(): ResponseEntity<Iterable<Subject>> = ResponseEntity.ok(subjectService.findAllSubjects())
+    fun getAllSubjects(): ResponseEntity<Iterable<SubjectDTO>> = ResponseEntity.ok(subjectService.findAllSubjects())
 
     @GetMapping("/{id}")
-    fun getStudentById(@PathVariable id: Long): ResponseEntity<Subject> {
+    fun getStudentById(@PathVariable id: Long): ResponseEntity<SubjectDTO> {
         val optional = subjectService.findSubjectById(id)
         return if (optional.isPresent)
             ResponseEntity.ok(optional.get())
         else
             ResponseEntity.notFound().build()
+    }
+
+    @PostMapping("/")
+    fun saveSubject(@RequestBody subjectDTO: SubjectDTO): ResponseEntity<Any> {
+        return try {
+            ResponseEntity.ok(subjectService.saveSubject(subjectDTO))
+        } catch (e: Exception){
+            ResponseEntity.badRequest().body(e.message)
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteSubjectById(@PathVariable id: Long): ResponseEntity<Any> {
+        return try {
+            subjectService.deleteSubjectById(id)
+            ResponseEntity.ok().build()
+        } catch (e: Exception){
+            ResponseEntity.badRequest().body(e.message)
+        }
     }
 }
---
+++ src/main/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/dto/SubjectDTO.kt
@@ -0,0 +1,3 @@
+package ua.edu.ukma.dudes.scheduleMeBaby.dto
+
+data class SubjectDTO(var id: Long?, var name: String?)
