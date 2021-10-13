39f8159090da3189f4b157082e3e379badfc8f15
BlaD200 <blad600@gmail.com>
2021-10-13 14:46:03 +0300

    Added example configuration

---
+++ src/main/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/security/WebSecurityConfigDev.kt
@@ -0,0 +1,25 @@
+package ua.edu.ukma.dudes.scheduleMeBaby.security
+
+import org.jetbrains.annotations.PropertyKey
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.web.servlet.invoke
+
+@EnableWebSecurity
+@ConditionalOnExpression("#{!environment.getProperty('spring.profiles.active').contains('dev')")
+class WebSecurityConfigDev : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity?) {
+        http {
+            httpBasic {}
+            authorizeRequests {
+                authorize("/**", permitAll)
+            }
+            csrf {
+                disable()
+            }
+        }
+    }
+}
--- src/main/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/security/WebSecurityConfigProd.kt
+++ src/main/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/security/WebSecurityConfigProd.kt
@@ -1,22 +1,19 @@
 package ua.edu.ukma.dudes.scheduleMeBaby.security
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.web.servlet.invoke
 
 @EnableWebSecurity
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+@ConditionalOnExpression("#{!environment.getProperty('spring.profiles.active').contains('prod')")
+class WebSecurityConfigProd : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
-        http {
-            httpBasic {}
-            authorizeRequests {
-                authorize("/**", permitAll)
-            }
-            csrf {
-                disable()
-            }
-        }
+        http!!
+        http.authorizeRequests()
+            .antMatchers("/login").permitAll()
+            .anyRequest().authenticated()
     }
 }
