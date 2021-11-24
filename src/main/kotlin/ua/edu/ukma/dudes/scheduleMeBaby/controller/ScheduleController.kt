afad49bfd426df012e0cf1985dbab946b455e36d
Iron Tony <lifegamecom1@gmail.com>
2021-11-24 15:46:26 +0200

    Added few basic pages

---
+++ src/main/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/controller/ScheduleController.kt
@@ -0,0 +1,18 @@
+package ua.edu.ukma.dudes.scheduleMeBaby.controller
+
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+
+@Controller
+@RequestMapping("/schedule")
+@Tag(name = "Schedule", description = "Schedule specific APIs")
+class ScheduleController {
+
+    @GetMapping("/")
+    fun books(): String {
+        return "schedule"
+    }
+}
