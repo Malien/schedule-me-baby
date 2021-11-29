cde543b0ca6835317a37147adf37709c29a41448
Iron Tony <lifegamecom1@gmail.com>
2021-11-29 23:25:56 +0200

    More fixes

--- src/main/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/service/StudentService.kt
+++ src/main/kotlin/ua/edu/ukma/dudes/scheduleMeBaby/service/StudentService.kt
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Service
 import ua.edu.ukma.dudes.scheduleMeBaby.dto.StudentDTO
 import ua.edu.ukma.dudes.scheduleMeBaby.dto.toDto
 import ua.edu.ukma.dudes.scheduleMeBaby.entity.Student
+import ua.edu.ukma.dudes.scheduleMeBaby.exception.InvalidArgumentException
 import ua.edu.ukma.dudes.scheduleMeBaby.exception.NotFoundException
 import ua.edu.ukma.dudes.scheduleMeBaby.repository.StudentRepository
 import java.util.*
 import javax.validation.constraints.NotBlank
 
-data class CreateStudentDTO(@NotBlank val name: String)
+data class CreateStudentDTO(@field:NotBlank val name: String)
 typealias UpdateStudentDTO = CreateStudentDTO
 
 @Service
@@ -23,10 +24,12 @@ class StudentService(private val studentRepository: StudentRepository) {
     fun createStudent(request: CreateStudentDTO): StudentDTO =
         studentRepository.save(Student(request.name)).toDto()
 
-    fun updateStudent(id: Long, patch: UpdateStudentDTO): StudentDTO {
+    fun updateStudent(id: Long, dto: UpdateStudentDTO): StudentDTO {
         val student = studentRepository.findById(id)
             .orElseThrow { NotFoundException("Cannot find student by id: $id") }
-        student.name = patch.name
+        if (dto.name[0].isLowerCase())
+            throw InvalidArgumentException("Student's name should not be lower case: " + dto.name)
+        student.name = dto.name
         return studentRepository.save(student).toDto()
     }
 
